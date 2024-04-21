0aa6a34e1a6070df79482af0b3a9671f68643fa2
BotikOfficial <134577891+BotikOfficial@users.noreply.github.com>
2024-04-21 14:11:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -18,77 +18,60 @@ let category1 = document.getElementById("category1");
 let category2 = document.getElementById("category2");
 let category3 = document.getElementById("category3");
 
-btn1.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 1!");
-		item = "1";
-		tg.MainButton.show();
-	}
-});
+// Функція для показу відповідних товарів при виборі категорії
+function showItems(category) {
+	let itemsContainer = document.getElementById("itemsContainer");
+	itemsContainer.innerHTML = ""; // Очистити контейнер перед виведенням нових товарів
 
-btn2.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
+	switch (category) {
+		case "Автотовари":
+			itemsContainer.innerHTML = `
+				<div class="item">
+					<img src="1.png" alt="" class="img">
+					<button class="btn" id="btn1">Add</button>
+				</div>
+				<div class="item">
+					<img src="2.png" alt="" class="img">
+					<button class="btn" id="btn2">Add</button>
+				</div>
+				<div class="item">
+					<img src="3.png" alt="" class="img">
+					<button class="btn" id="btn3">Add</button>
+				</div>
+			`;
+			break;
+		case "Дім та сад":
+			itemsContainer.innerHTML = `
+				<div class="item">
+					<img src="4.png" alt="" class="img">
+					<button class="btn" id="btn4">Add</button>
+				</div>
+				<div class="item">
+					<img src="5.png" alt="" class="img">
+					<button class="btn" id="btn5">Add</button>
+				</div>
+			`;
+			break;
+		case "Електроніка":
+			itemsContainer.innerHTML = `
+				<div class="item">
+					<img src="6.png" alt="" class="img">
+					<button class="btn" id="btn6">Add</button>
+				</div>
+			`;
+			break;
+		default:
+			break;
 	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 2!");
-		item = "2";
-		tg.MainButton.show();
-	}
-});
+}
 
-btn3.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 3!");
-		item = "3";
-		tg.MainButton.show();
-	}
-});
-
-btn4.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 4!");
-		item = "4";
-		tg.MainButton.show();
-	}
-});
-
-btn5.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 5!");
-		item = "5";
-		tg.MainButton.show();
-	}
-});
-
-btn6.addEventListener("click", function(){
-	if (tg.MainButton.isVisible) {
-		tg.MainButton.hide();
-	}
-	else {
-		tg.MainButton.setText("Вы выбрали товар 6!");
-		item = "6";
-		tg.MainButton.show();
-	}
-});
-
+// Обробники подій для кнопок категорій
 category1.addEventListener("click", function(){
 	alert("Ви вибрали категорію 'Автотовари'!");
 	item = "Автотовари";
 	tg.MainButton.setText("Ви вибрали категорію 'Автотовари'!");
 	tg.MainButton.show();
+	showItems(item); // Показувати відповідні товари
 });
 
 category2.addEventListener("click", function(){
@@ -96,6 +79,7 @@ category2.addEventListener("click", function(){
 	item = "Дім та сад";
 	tg.MainButton.setText("Ви вибрали категорію 'Дім та сад'!");
 	tg.MainButton.show();
+	showItems(item); // Показувати відповідні товари
 });
 
 category3.addEventListener("click", function(){
@@ -103,6 +87,36 @@ category3.addEventListener("click", function(){
 	item = "Електроніка";
 	tg.MainButton.setText("Ви вибрали категорію 'Електроніка'!");
 	tg.MainButton.show();
+	showItems(item); // Показувати відповідні товари
+});
+
+// Обробники подій для кнопок товарів
+document.addEventListener("click", function(event) {
+	if (event.target.classList.contains("btn")) {
+		let selectedItemId = event.target.id;
+		switch (selectedItemId) {
+			case "btn1":
+				alert("Ви вибрали товар 1!");
+				break;
+			case "btn2":
+				alert("Ви вибрали товар 2!");
+				break;
+			case "btn3":
+				alert("Ви вибрали товар 3!");
+				break;
+			case "btn4":
+				alert("Ви вибрали товар 4!");
+				break;
+			case "btn5":
+				alert("Ви вибрали товар 5!");
+				break;
+			case "btn6":
+				alert("Ви вибрали товар 6!");
+				break;
+			default:
+				break;
+		}
+	}
 });
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
